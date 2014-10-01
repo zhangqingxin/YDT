b4c09d8ec9ebef8e9bb9ea0c3d256165c9c197e8
zhangqingxin <zhangqingxin@dianxinos.com>
2014-10-01 22:49:54 +0800

    feature: 增加用电常识模块

---
+++ app/controllers/YongDianChangShiAdmin.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class YongDianChangShiAdmin extends Controller {
+
+	public static void index() {
+		render();
+	}
+
+	public static void jumin_jibenqingkuang() {
+		render();
+	}
+}
