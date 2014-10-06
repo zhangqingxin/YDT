152907608e56f7aa8802affebe461977fd29401e
zhangqingxin <zhangqingxin@dianxinos.com>
2014-10-07 00:18:04 +0800

    完成用电常识模块

--- app/controllers/YongDianChangShiAdmin.java
+++ app/controllers/YongDianChangShiAdmin.java
@@ -31,4 +31,52 @@ public class YongDianChangShiAdmin extends Controller {
 	public static void jumin_weixiuweihu() {
 		render();
 	}
+	
+	public static void geti_jibenqingkuang() {
+		render();
+	}
+	
+	public static void geti_jieyueyongdian() {
+		render();
+	}
+	
+	public static void geti_anquanyongdian() {
+		render();
+	}
+	
+	public static void geti_yongdianbiangeng() {
+		render();
+	}
+	
+	public static void geti_dianfeidianjia() {
+		render();
+	}
+	
+	public static void geti_weixiuweihu() {
+		render();
+	}
+	
+	public static void bangong_jibenqingkuang() {
+		render();
+	}
+	
+	public static void bangong_jieyueyongdian() {
+		render();
+	}
+	
+	public static void bangong_anquanyongdian() {
+		render();
+	}
+	
+	public static void bangong_yongdianbiangeng() {
+		render();
+	}
+	
+	public static void bangong_dianfeidianjia() {
+		render();
+	}
+	
+	public static void bangong_weixiuweihu() {
+		render();
+	}
 }
