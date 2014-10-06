e2a179b1cfb8f6da0f66766a63b3bbeaad8fd419
zhangqingxin <zhangqingxin@dianxinos.com>
2014-10-06 20:57:59 +0800

    增加用电常识部分内容

--- app/controllers/YongDianChangShiAdmin.java
+++ app/controllers/YongDianChangShiAdmin.java
@@ -11,4 +11,24 @@ public class YongDianChangShiAdmin extends Controller {
 	public static void jumin_jibenqingkuang() {
 		render();
 	}
+
+	public static void jumin_dianfeidianjia() {
+		render();
+	}
+	
+	public static void jumin_anquanyongdian() {
+		render();
+	}
+	
+	public static void jumin_jieyueyongdian() {
+		render();
+	}
+	
+	public static void jumin_yongdianbiangeng() {
+		render();
+	}
+	
+	public static void jumin_weixiuweihu() {
+		render();
+	}
 }
