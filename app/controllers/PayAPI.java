cf20f7ba300820edfca809391a2d41713216f3c0
lzfying <lzfying@gmail.com>
2014-10-06 22:48:20 +0800

    tiaoshi

--- app/controllers/PayAPI.java
+++ app/controllers/PayAPI.java
@@ -220,8 +220,9 @@ public class PayAPI extends Controller{
 				//判断该笔订单是否在商户网站中已经做过处理
 					//如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
 					//如果有做过处理，不执行商户的业务程序
-				verifyOK(out_trade_no,"returnurl");
 				Logger.logInfo("###################  laishi " );
+				verifyOK(out_trade_no,"returnurl");
+				
 				//该页面可做页面美工编辑
 				renderHtml("验证成功<br />");
 				
